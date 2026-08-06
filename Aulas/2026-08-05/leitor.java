0c547aba791321b5b2bdafa98544fc16b585a4c0
MauricioTellesSilva <mauriciotellessilva@gmail.com>
2026-08-05 21:34:08 -0300

    create leitor

--- Aulas/2026-08-05/leitor.java
+++ Aulas/2026-08-05/leitor.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
 
 public class leitor {
     public static void main (String [] args){
-
+        Scanner leitor = new Scanner(System.in);
+        String linha;
+        while (leitor.hasNext()){
+        linha = leitor.nextLine();
+        IO.println("Saida: "+linha);
+        }    
+        leitor.close();
     }
 }
