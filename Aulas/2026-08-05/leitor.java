9632d5c32e01a5322cc4313e30fa2a916cc20bfc
MauricioTellesSilva <mauriciotellessilva@gmail.com>
2026-08-05 21:49:54 -0300

    2 versão do leitor

--- Aulas/2026-08-05/leitor.java
+++ Aulas/2026-08-05/leitor.java
@@ -4,10 +4,20 @@ public class leitor {
     public static void main (String [] args){
         Scanner leitor = new Scanner(System.in);
         String linha;
-        while (leitor.hasNext()){
-        linha = leitor.nextLine();
-        IO.println("Saida: "+linha);
-        }    
+        while(leitor.hasNextLine()){
+            linha=leitor.nextLine();
+            String[] aluno =linha.split(",");
+            double nota1=Double.parseDouble(aluno[1]);
+            double nota2=Double.parseDouble(aluno[2]);
+            double media=Math.round((nota1+nota2)/2);
+            String estado="";
+            if (media>=6){
+                estado="aprovado";
+            } else {
+                estado="reprovado";
+            }
+            IO.println("A media do aluno/aluna "+aluno[0]+ " foi "+media+" ele está "+estado);
+        }   
         leitor.close();
     }
 }
